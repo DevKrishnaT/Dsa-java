1c17d99e67a3ab587dfde0677087c89750b9eb75
DevKrishnaT <krishnasehai1@gmail.com>
2026-01-07 13:16:18 +0530

    some more practice 1

---
+++ lession_1/topic_group_1/questions/basic_q/BasicMathsq/armsStrong2.java
@@ -0,0 +1,19 @@
+package lession_1.topic_group_1.questions.basic_q.BasicMathsq;
+
+public class armsStrong2 {
+    public static void main(String[] args) {
+        int n = 153;
+        int copy = n;
+        int output = 0;
+        int power = String.valueOf(n).length();
+        while (n > 0) {
+            int temp = n % 10;
+            n /= 10;
+            temp = (int) Math.pow(temp, power);
+            output += temp;
+
+        }
+       
+        System.out.println(copy == output);
+    }
+}
