634afd966f01eee033888b53ad2a61b0d788217e
DevKrishnaT <krishnasehai1@gmail.com>
2026-02-06 20:21:46 +0530

    bitmanuplation

---
+++ lesson_1/BitManupulation/IntegerTobit.java
@@ -0,0 +1,26 @@
+package lesson_1.BitManupulation;
+
+public class IntegerTobit {
+    static void main(String[] args) {
+        int n = 15;
+        String ans = Covert(n);
+        System.out.println(ans);
+
+    }
+
+    private static String Covert(int n) {
+        StringBuilder res = new StringBuilder();
+
+        while (n != 1) {
+            if (n % 2 == 1) res.append("1");
+            else res.append("0");
+            n = n / 2;
+
+        }
+        res.append("1");
+        res.reverse();
+
+        return res.toString();
+    }
+
+}
