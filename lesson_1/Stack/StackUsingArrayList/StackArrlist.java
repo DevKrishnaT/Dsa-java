1371cb486b238212a0a8d90c2735d5fb0412b1c8
DevKrishnaT <krishnasehai1@gmail.com>
2026-02-10 14:25:03 +0530

    Stack

---
+++ lesson_1/Stack/StackUsingArrayList/StackArrlist.java
@@ -0,0 +1,22 @@
+package lesson_1.Stack.StackUsingArrayList;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StackArrlist {
+    class Stack {
+        static List<Integer> ans = new ArrayList<>();
+        public static void Push()
+
+        static void main(String[] args) {
+
+        }
+
+    }
+
+
+}
+
+
+
