615e9b473c30e691a1129e63cb338400343b6c06
DevKrishnaT <krishnasehai1@gmail.com>
2026-02-13 21:28:07 +0530

    Infix/prefix/postfix Conversions

---
+++ lesson_1/questions/Longest.java
@@ -0,0 +1,30 @@
+package lesson_1.questions;
+
+import java.util.Arrays;
+
+public class Longest {
+    static void main(String[] args) {
+        int[] arr = {2, 6, 1, 9, 4, 5, 3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        int counter = 0;
+        int longest = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int num = arr[i];
+            int j = i + 1;
+            while (arr[j++] == num + 1) {
+                counter++;
+
+            }
+
+            longest = Math.max(counter, longest);
+
+
+        }
+        System.out.println(longest);
+    }
+
+}
