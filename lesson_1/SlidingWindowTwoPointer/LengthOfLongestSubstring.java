9122753eab8a00b729728de559fe09481802a4f4
DevKrishnaT <krishnasehai1@gmail.com>
2026-02-18 21:34:17 +0530

    sliding Window

---
+++ lesson_1/SlidingWindowTwoPointer/LengthOfLongestSubstring.java
@@ -0,0 +1,34 @@
+package lesson_1.SlidingWindowTwoPointer;
+
+import java.nio.charset.CharsetEncoder;
+import java.util.HashMap;
+
+public class LengthOfLongestSubstring {
+    static void main(String[] args) {
+        String s = "abcabcbb";
+        int ans = lengthOfLongestSubstring(s);
+    }
+
+    private static int lengthOfLongestSubstring(String s) {
+        int left = 0;
+
+        int maxLength = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int right = 0; right < s.length(); right++) {
+
+
+            char current = s.charAt(right);
+
+
+            if (map.containsKey(current) && map.get(current) >= left) {
+                left = map.get(current) + 1;
+            }
+
+            map.put(current, right);
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
