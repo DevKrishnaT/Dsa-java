1eacf4bb9d4561579f8da7ae70a3e38d32a42b35
DevKrishnaT <krishnasehai1@gmail.com>
2026-02-20 21:46:57 +0530

    more q

---
+++ lesson_1/SlidingWindowTwoPointer/MaxScore.java
@@ -0,0 +1,31 @@
+package lesson_1.SlidingWindowTwoPointer;
+
+public class MaxScore {
+    static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 1};
+        int k = 3;
+        int ans = maxScore(nums, k);
+        System.out.println(ans);
+    }
+
+    private static int maxScore(int[] nums, int k) {
+        int n = nums.length;
+        int totalSum = 0;
+        for (int i : nums) totalSum += i;
+        if (k == n) return totalSum;
+
+        int minWindow = n - k;
+        int WindowSum = 0;
+        for (int i = 0; i < minWindow; i++) {
+            WindowSum += nums[i];
+        }
+        int minWindowSum = WindowSum;
+        for (int i = minWindow; i < n; i++) {
+            WindowSum += nums[i] - nums[i - minWindow];
+            minWindowSum = Math.min(minWindowSum, WindowSum);
+        }
+
+        return totalSum - minWindowSum;
+
+    }
+}
