77eb808970246bc9cd4f724b6f64ee2ba8144c52
DevKrishnaT <krishnasehai1@gmail.com>
2026-02-22 20:29:58 +0530

    Correct the folder structure

---
+++ Heap/Implimentation/HeapPrinter.java
@@ -0,0 +1,27 @@
+package Heap.Implimentation;
+
+import java.util.List;
+
+public class HeapPrinter {
+    public static void printTree(List<Integer> heap) {
+        if (heap == null || heap.isEmpty()) {
+            System.out.println("(empty heap)");
+            return;
+        }
+
+        int level = 0;
+        int index = 0;
+
+        while (index < heap.size()) {
+            int nodesInLevel = (int) Math.pow(2, level);
+
+            for (int i = 0; i < nodesInLevel && index < heap.size(); i++) {
+                System.out.print(heap.get(index) + " ");
+                index++;
+            }
+
+            System.out.println();
+            level++;
+        }
+    }
+}
---
+++ Heap/Implimentation/MainMinHeap.java
@@ -0,0 +1,19 @@
+package Heap.Implimentation;
+
+
+public class MainMinHeap {
+    static void main(String[] args) {
+        MinHeap heap = new MinHeap();
+        heap.insert(10);
+        heap.insert(24);
+        heap.insert(14);
+        heap.insert(49);
+        heap.insert(43);
+        heap.insert(243);
+        heap.insert(23);
+        heap.insert(94);
+        heap.insert(12);
+        heap.insert(493);
+        HeapPrinter.printTree(heap.getHeap());
+    }
+}
