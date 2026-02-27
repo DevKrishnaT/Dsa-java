a341a38363791f291f62ad1c8839440ce808e325
DevKrishnaT <krishnasehai1@gmail.com>
2026-02-27 23:17:09 +0530

    Greedy Algo q

--- GreedyAlgo/MaxMeetings.java
+++ GreedyAlgo/MaxMeetings.java
@@ -1,5 +1,7 @@
 package GreedyAlgo;
 
+import java.util.Arrays;
+
 public class MaxMeetings {
     static void main(String[] args) {
         int[] start = {1, 3, 0, 5, 8, 5};
@@ -9,15 +11,25 @@ public class MaxMeetings {
     }
 
     private static int maxMeetings(int[] start, int[] end) {
-        boolean[] check = new boolean[start.length];
-        int count = 0;
-        check[start.length - 1] = true;
-        for (int i = 0; i < start.length - 1; i++) {
-            check[i] = end[i] < start[i + 1];
+        int n = start.length;
+        int[][] meetings = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            meetings[i][0] = start[i];
+            meetings[i][1] = end[i];
         }
 
-        for (boolean ans : check) {
-            if (ans) count++;
+        Arrays.sort(meetings, (a, b) -> a[1] - b[1]);
+
+        int count = 1;
+        int lastEnd = meetings[0][1];
+
+        for (int i = 1; i < n; i++) {
+
+            if (meetings[i][0] > lastEnd) {
+                count++;
+                lastEnd = meetings[i][1];
+            }
         }
         return count;
     }
