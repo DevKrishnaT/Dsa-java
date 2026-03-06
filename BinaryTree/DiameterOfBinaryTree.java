dfa83b9c039ab317f03426c3194eb6531d0f4950
DevKrishnaT <krishnasehai1@gmail.com>
2026-03-06 08:27:20 +0530

    binary tree

---
+++ BinaryTree/DiameterOfBinaryTree.java
@@ -0,0 +1,30 @@
+package BinaryTree;
+
+public class DiameterOfBinaryTree {
+    static int maxi;
+
+    static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        int max = 0;
+        int ans = find(root);
+
+        System.out.println(maxi);
+    }
+
+    public static int find(TreeNode root) {
+        if (root == null) return 0;
+
+
+        int left = find(root.left);
+
+        int right = find(root.right);
+
+
+        maxi = Math.max(maxi, left + right);
+        return 1 + Math.max(left, right);
+    }
+}
