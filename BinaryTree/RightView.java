acf46bdd298fa4124bd11cc6530e357d11949d9b
DevKrishnaT <krishnasehai1@gmail.com>
2026-03-12 19:07:19 +0530

    binary tree

--- BinaryTree/RightView.java
+++ BinaryTree/RightView.java
@@ -3,15 +3,7 @@ package BinaryTree;
 import java.util.*;
 
 public class RightView {
-    static class pair {
-        int hight;
-        TreeNode node;
 
-        pair(int hight, TreeNode node) {
-            this.hight = hight;
-            this.node = node;
-        }
-    }
 
     static void main(String[] args) {
         TreeNode root = new TreeNode(1);
@@ -27,27 +19,30 @@ public class RightView {
     }
 
     private static List<Integer> rightSideView(TreeNode root) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        Queue<pair> queue = new LinkedList<>();
-        queue.offer(new pair(0, root));
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
         while (!queue.isEmpty()) {
-            pair p = queue.poll();
+            int size = queue.size();
 
-            if (!map.containsKey(Math.abs(p.hight))) {
-                map.put(Math.abs(p.hight), p.node.val);
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (i == size - 1) {
+                    result.add(node.val);
+                }
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
             }
-            if (p.node.right != null) {
-                queue.add(new pair(p.hight + 1, p.node.right));
-            }
-
-            if (p.node.left != null) {
-                queue.add(new pair(p.hight - 1, p.node.left));
-            }
-
-
         }
 
-        return new ArrayList<>(map.values());
+        return result;
+
+
     }
 
 }
