95232d42e6fb178200a19ce942fab87694193d6c
DevKrishnaT <krishnasehai1@gmail.com>
2026-04-05 14:34:12 +0530

    some leetcode q

---
+++ twoPointer/removeDuplicatesII.java
@@ -0,0 +1,23 @@
+package twoPointer;
+
+public class removeDuplicatesII {
+    static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int ans = removeDuplicates(nums);
+        System.out.println(ans);
+
+    }
+
+    private static int removeDuplicates(int[] nums) {
+        int i = 0;
+
+        for (int n : nums) {
+            if (i < 2 || n != nums[i - 2]) {
+                nums[i] = n;
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
