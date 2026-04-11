8146f3d0f9f5ca28ee6d4ca59f55cfa9bc564849
DevKrishnaT <krishnasehai1@gmail.com>
2026-04-11 17:23:12 +0530

    some leetcode q

---
+++ BitManupulation/SmallestSetBit.java
@@ -0,0 +1,19 @@
+package BitManupulation;
+
+public class SmallestSetBit {
+    static void main(String[] args) {
+        int n = 5;
+        int ans = smallest(n);
+    }
+
+    private static int smallest(int n) {
+        int k = 1;
+        while (true) {
+            int num = (1 << k) - 1;
+            if (num >= n) {
+                return num;
+            }
+            k++;
+        }
+    }
+}
