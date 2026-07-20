0554726fbe7052f710091ef8f30579223ad4fb0e
DevKrishnaT <krishnasehai1@gmail.com>
2026-07-20 22:50:58 +0530

    revision

--- twoPointer/TrappingRainWater.java
+++ twoPointer/TrappingRainWater.java
@@ -4,24 +4,34 @@ public class TrappingRainWater {
     static void main(String[] args) {
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         int ans = trap(height);
+        System.out.println(ans);
     }
 
     private static int trap(int[] nums) {
         int left = 0;
 
-        while (left < nums.length) {
-            if (nums[left] == 0) {
-                left++;
-            }
-            int BrickBt = 0;
-            int totalSum = 0;
-            int right = left + 1;
-            while (right < nums.length && nums[left] > nums[right]) {
-                BrickBt += nums[right];
-            }
+        int right = nums.length - 1;
+        int maxl = 0;
+        int maxr = 0;
+        int total = 0;
 
+        while (left < right) {
+            if (nums[left] <= nums[right]) {
+                int ans = maxl - nums[left];
+                if (!(ans <= 0)) {
+                    total += ans;
+                }
+                maxl = Math.max(maxl, nums[left++]);
+            } else {
+                int ans = maxr - nums[right];
+                if (!(ans <= 0)) {
+                    total += ans;
+                }
+                maxr = Math.max(maxr, nums[right--]);
+
+            }
         }
-        return 0;
+        return total;
 
     }
 }
