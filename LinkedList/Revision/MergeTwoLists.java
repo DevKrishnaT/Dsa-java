37db9f8bd3e7d0e96f8ed8535053410e236c655c
DevKrishnaT <krishnasehai1@gmail.com>
2026-08-05 06:52:57 +0530

    revision

---
+++ LinkedList/Revision/MergeTwoLists.java
@@ -0,0 +1,52 @@
+package LinkedList.Revision;
+
+import static LinkedList.Revision.LinkedList.treverse;
+
+public interface MergeTwoLists {
+    static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode ansList = mergeTwoList(list1, list2);
+        treverse(ansList);
+    }
+
+    static ListNode mergeTwoList(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+                curr = curr.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+                curr = curr.next;
+            }
+        }
+
+        if (list1 != null) {
+            curr.next = list1;
+        }
+
+        if (list2 != null) {
+            curr.next = list2;
+        }
+
+
+        return dummy.next;
+
+
+    }
+}
