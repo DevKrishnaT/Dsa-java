378ead9ed14fc71cc573b3e6c393e48ef9a41fa5
DevKrishnaT <krishnasehai1@gmail.com>
2026-08-20 16:15:30 +0530

    revision

---
+++ Queue/Revision/FirstUniqChar.java
@@ -0,0 +1,29 @@
+package Queue.Revision;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FirstUniqChar {
+    static void main(String[] args) {
+        String s = "etcode";
+        int ans = firstUniqChar(s);
+        System.out.println(ans);
+    }
+
+    private static int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
