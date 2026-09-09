f1dad7f55a955b7a6f3178c7f7853019b8c84823
DevKrishnaT <krishnasehai1@gmail.com>
2026-09-09 21:19:35 +0530

    queue implementation revision

---
+++ test/NextGreater.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class NextGreater {
+    static void main(String[] args) {
+        int[] nums2 = {1, 3, 4, 2};
+        int[] nums1 = {4, 1, 2};
+
+        int[] ans = findNextGreater(nums1, nums2);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    private static int[] findNextGreater(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+                stack.pop();
+            }
+
+            if (stack.isEmpty()) {
+                map.put(nums2[i], -1);
+            } else {
+                map.put(nums2[i], stack.peek());
+            }
+            stack.push(nums2[i]);
+        }
+
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+}
